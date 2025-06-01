d598b45c70ea9685ba67749a514d03ee1e9047c4
Ваше Ім'я <_email@example.com>
2025-06-01 03:41:27 +0200

    Додано Explore Mode (ExploreFeed компонент)

--- hellolove-vite-main/vite.config.js
+++ hellolove-vite-main/vite.config.js
@@ -1,9 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000, // встановіть порт, якщо 5173 та 5174 не працюють
-  },
-})
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        sw: 'firebase-messaging-sw.js' // важливо!
+      }
+    }
+  }
+});
